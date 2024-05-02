bfb597cff310b8cebca0806c2662d3cffbcc59d4
Martin Bjærge Jensen <martin.martinjensen@gmail.com>
2024-05-02 09:13:37 +0200

    add try/catch and logging (#2)

--- mergin-py-sync.py
+++ mergin-py-sync.py
@@ -1,5 +1,12 @@
 import os
+import sys
+import logging
 import mergin
+
+logger = logging.getLogger('')
+logger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler(sys.stdout)
+logger.addHandler(sh)
 
 url = os.getenv('MERGIN_URL')
 login = os.getenv('MERGIN_USERNAME')
@@ -11,7 +18,12 @@ projects = client.projects_list()
 for project in projects:
     project_path = os.path.join(project['namespace'], project['name'])
     dest_path = os.path.join(data, project_path)
-    if os.path.exists(dest_path):
-        client.pull_project(dest_path)
-    else:
-        client.download_project(project_path=project_path, directory=dest_path)
+    logger.info(f"processing {dest_path}")
+    try:
+        if os.path.exists(dest_path):
+            client.pull_project(dest_path)
+        else:
+            client.download_project(project_path=project_path, directory=dest_path)
+    except Exception as e:
+        logger.warning(f"Error processing {dest_path}")       
+        logger.warning(e)
